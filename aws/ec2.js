9a856fd05583ae46be89c85809fa675bd2e4ad7b
BSA <bsackerman3@gmail.com>
2022-11-24 00:57:37 -0800

    Update ec2.js

--- aws/ec2.js
+++ aws/ec2.js
@@ -28,10 +28,11 @@ module.exports.describeTags = async (configState=configStateDefault) => {
   var params = configState.AWS.ec2.params;
   var data = await ec2.describeTags(params, function(err, data) {
     if (err) console.log(err, err.stack); // an error occurred
-    else { console.log("data:" + JSON.stringify(data, null, 2)) 
+    else { 
+      console.log("data:" + JSON.stringify(data, null, 2)); 
     }
-  }).promise().then(data => {return data});
-  return data
+  }).promise().then(data => {return data;});
+  return data;
 
    /*
     data = {
@@ -57,11 +58,11 @@ module.exports.AutoScalingGroupName = async (configState=configStateDefault) => 
 // node -p -e 'require("./scripts/ec2.js").AutoScalingGroupName({ "AWS": { config: {region: "us-east-1"}, ec2: { params: { Filters: [ {  Name: "resource-id", Values: [ "i-03d9327bdd247f9b2"] } ] } }  }}).then(AutoScalingGroupName => {console.log("AutoScalingGroupName:"+JSON.stringify(AutoScalingGroupName, null, 2) )})'
     var AutoScalingGroupName = exports.describeTags( configState ).then(data => {
         //console.log("data:" + JSON.stringify(data, null, 2))
-        configState.autoscalingObject = data.Tags.find(o => o.Key === 'aws:autoscaling:groupName')
+        configState.autoscalingObject = data.Tags.find(o => o.Key === 'aws:autoscaling:groupName');
         //console.log(configState.autoscalingObject.Value);
-        return configState.autoscalingObject.Value
-    }).then(AutoScalingGroupName => { return AutoScalingGroupName });
-    return AutoScalingGroupName
+        return configState.autoscalingObject.Value;
+    }).then(AutoScalingGroupName => { return AutoScalingGroupName; });
+    return AutoScalingGroupName;
 };
 
 return 0;
